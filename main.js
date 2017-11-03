c5765df7cee54a5a82eaffe2c3cb02d0a866bbfc
gtht4242 <gtht4242@gmail.com>
2017-11-04 10:43:24 +1100

    Implement room loading from mapData

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-var game = new Phaser.Game('99', '99', Phaser.AUTO, "", {preload: preload, create: create, update: update});
+const game = new Phaser.Game('99', '99', Phaser.AUTO, "", {preload: preload, create: create, update: update});
 function preload() {
-    // Set constants
+    createMap();
+    // Set Phaser constants
     game.stage.backgroundColor = "#ffffff";
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     // Load images
     game.load.image('player_black', 'images/square_black.png');
-    game.load.image('wall_cyan', 'images/rect_cyan.png');
+    game.load.image('wall_cyan_horizontal', 'images/rect_cyan_hori.png');
+    game.load.image('wall_cyan_vertical', 'images/rect_cyan_vert.png');
+    game.load.image('wall_cyan_square', 'images/square_cyan.png');
+    game.load.image('wall_green_horizontal', 'images/rect_green_hori.png');
+    game.load.image('wall_green_vertical', 'images/rect_green_vert.png');
+    game.load.image('wall_green_square', 'images/square_green.png');
+    game.load.image('wall_red_horizontal', 'images/rect_red_hori.png');
+    game.load.image('wall_red_vertical', 'images/rect_red_vert.png');
+    game.load.image('wall_red_square', 'images/square_red.png');
+    game.load.image('wall_yellow_horizontal', 'images/rect_yellow_hori.png');
+    game.load.image('wall_yellow_vertical', 'images/rect_yellow_vert.png');
+    game.load.image('wall_yellow_square', 'images/square_yellow.png');
 }
 function create() {
     // Initialise core systems
     game.physics.startSystem(Phaser.Physics.ARCADE);
     cursors = game.input.keyboard.createCursorKeys();
-    // Create walls
+    // Create walls group and load starting room
     walls = game.add.group();
     walls.enableBody = true;
-    var upWall = walls.create(0, 0, 'wall_cyan');
-    var downWall = walls.create(0, game.world.height - 100, 'wall_cyan');
-    upWall.body.immovable = true;
-    downWall.body.immovable = true;
+    loadRoom();
     // Create wall emitter
     wallEmitter = game.add.emitter(0, 0, 10);
     wallEmitter.makeParticles('player_black');
@@ -31,6 +40,8 @@ function create() {
     player.anchor.y = 0.5;
     player.body.maxVelocity.x = 400;
     player.body.maxVelocity.y = 400;
+    player.checkWorldBounds = true;
+    player.events.onOutOfBounds.add(roomChange);
 }
 function update() {
     // Check for wall collision
@@ -57,7 +68,34 @@ function update() {
         player.body.velocity.y += 15;
 }
 function wallParticles(player) {
+    // Emit wall particles at current location
     wallEmitter.x = player.body.x;
     wallEmitter.y = player.body.y;
     wallEmitter.start(true, 500, null, 10);
+}
+function loadRoom() {
+    // Loads variables for room
+    room = mapData[roomIndex];
+    walls.forEach(function(child) {child.kill();});
+    for (var i = 0; i < room.walls.length; i++) {
+        var wall = walls.create(room.walls[i][0], room.walls[i][1], room.walls[i][2]);
+        wall.body.immovable = true;
+    }
+}
+function roomChange() {
+    // Change roomIndex then call loadRoom
+    if (player.body.x < 0) {
+        roomIndex -= 1;
+        player.x += game.world.width;
+    } else if (player.body.x > game.world.width) {
+        roomIndex += 1;
+        player.x -= game.world.width;
+    } else if (player.body.y < 0) {
+        roomIndex -= mapWidth;
+        player.y += game.world.height;
+    } else if (player.body.y > game.world.height) {
+        roomIndex += mapWidth;
+        player.y -= game.world.height;
+    }
+    loadRoom();
 }
